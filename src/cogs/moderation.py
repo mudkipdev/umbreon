5c4167c093d078073f3c1e5f12ac6d56c6ebc22e
mudkipdev <86132148+mudkipdev@users.noreply.github.com>
2022-03-19 21:11:35 -0600

    moderation cog

---
+++ src/cogs/moderation.py
@@ -0,0 +1,39 @@
+from discord.ext import commands
+import discord
+
+class ModerationCog(commands.Cog):
+    """Useful commands for moderators of a Discord server."""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(help = "Temporarily removes a member from the server.")
+    async def kick(self, ctx, members: commands.Greedy[discord.Member], *, reason: str = ''):
+        for member in members:
+            await member.kick(reason=reason)
+
+        wording = ' have been kicked from the server.' if len(members) > 1 else ' has been kicked from the server.'
+        embed = discord.Embed(
+            color = self.bot.config['colors']['moderation'],
+            description = ', '.join([member.name for member in members]) + wording
+        )
+
+        embed.set_author(name = 'Moderation', icon_url = self.bot.config['icons']['moderation'])
+        await ctx.reply(embed=embed)
+
+    @commands.command(help = "Permanently removes a member from the server.")
+    async def ban(self, ctx, members: commands.Greedy[discord.Member], *, reason: str = ''):
+        for member in members:
+            await member.ban(reason=reason)
+
+        wording = ' have been banned from the server.' if len(members) > 1 else ' has been banned from the server.'
+        embed = discord.Embed(
+            color = self.bot.config['colors']['moderation'],
+            description = ', '.join([member.name for member in members]) + wording
+        )
+
+        embed.set_author(name = 'Moderation', icon_url = self.bot.config['icons']['moderation'])
+        await ctx.reply(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(ModerationCog(bot))
