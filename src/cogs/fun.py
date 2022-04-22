e13f27d89e5f79271d4bf451db1c578af208f92b
mudkipdev <86132148+mudkipdev@users.noreply.github.com>
2022-04-21 20:54:00 -0600

    chore: refactor fun cog

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -5,64 +5,60 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 
+MINECRAFT_ENDPOINT = "https://api.mcsrvstat.us/2/{}"
 
 class Fun(commands.Cog):
-    """A Cog dedicated to various Fun Commands"""
+    """A cog dedicated to various fun commands such as 8 ball."""
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="8ball",
-                      aliases=["eight_ball", "eightball", "8-ball"],
-                      )
-    async def eight_ball(self, ctx):
-        """Answers a yes/no question."""
-        possible_responses = [
-            "That is a resounding no",
-            "It is not looking likely",
-            "Too hard to tell",
-            "It is quite possible",
-            "Definitely",
-            "Why don\'t you figure it out yourself you lazy motherfucker",
-            "Yes, but do it drunk as fuck!",
-            "My sources say no, but they also said Hillary would win.",
-            "Do not swipe right, it\'s your cousin",
-            "Do swipe right, it\'s your cousin!",
-            "Do what Jesus would do, die at the age of 33",
-            "Trump uses me when deciding to go to war"
+        with open("res/dadjokes.txt").readlines() as jokes:
+            self.jokes = jokes
 
-        ]
-        em = discord.Embed(color=self.bot.config["colors"]["default"])
-        em.title = ":mage: The 8 ball wizard says..."
-        em.description = (random.choice(possible_responses) + ", " + ctx.author.mention)
+        with open("res/8ball.txt").readlines() as _8ball_responses:
+            self._8ball_responses = _8ball_responses
+
+    @commands.command(aliases = ["joke"])
+    async def dadjoke(self, ctx):
+        embed = discord.Embed(title="Joke", description=random.choice(self.jokes).split("<>"))
+        await ctx.reply(embed = embed)
+
+    @commands.command(name="8ball", aliases=["eightball"], help="Helps you answer a yes/no question.")
+    async def _8ball(self, ctx):
+        embed = discord.Embed(color=self.bot.config["colors"]["default"])
+        embed.title = ":8ball: The 8 ball says..."
+        embed.description = random.choice(self._8ball_responses) + ", " + ctx.author.mention
         await ctx.send(embed=em)
 
-    @commands.command()
+    @commands.command(help = "Get the bot's response time.")
     async def ping(self, ctx):
-        """Pong! Get the bots response time"""
         em = discord.Embed(color=self.bot.config["colors"]["default"])
         em.title = ":ping_pong: Pong!"
         em.description = f"{round(self.bot.latency * 1000)} ms"
         await ctx.send(embed=em)
 
-    @commands.command(aliases=["minecraft"])
-    async def mc(self, ctx, url):
-        """Check the status of your favorite Minecraft Server!"""
-        if url is None:
-            return await ctx.send("Please provide a valid server URL or IP!")
-        else:
-            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as cs:
-                async with cs.get(f"https://api.mcsrvstat.us/2/{url}") as r:
-                    resp = await r.json()
-            embed = discord.Embed(title=f"Server status for {url}",
-                                  color=self.bot.config["colors"]["default"],
-                                  timestamp=datetime.utcnow())
-            if resp["online"]:
-                embed.description = f"Server Online!!! \n Players:{resp['players']['online']}"
-                await ctx.send(embed=embed)
+    @commands.command(aliases=["mc"])
+    async def minecraft(self, ctx, ip):
+        """Check the uptime of your favorite Minecraft server."""
+        async with self.bot.client_session.get(MINECRAFT_ENDPOINT.format(ip)) as r:
+            resp = await r.json()
 
-            else:
-                embed.description = "Server is Offline!"
-                await ctx.send(embed=embed)
+        embed = discord.Embed(
+            title=f"{ip} Status",
+            color=self.bot.config["colors"]["default"],
+            timestamp=datetime.utcnow()
+        )
+
+        embed.description = "Server is online." if resp["online"] else "Server is offline."
+
+        if resp.get("motd"):
+            motd = "```\n" + "\n".join(resp["motd"]["clean"]) + "```"
+            embed.add_field(name="MOTD", value=motd)
+
+        if resp.get("players"):
+            embed.add_field(name="Players", value=f"{resp['players']['online']}/{resp['players']['max']}", inline = False)
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
